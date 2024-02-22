ef7c7f81daa3e29b81a9b726b06d3d5bfef76138
Glasteroid <kennethangeles880@yahoo.com>
2024-02-22 11:18:21 -0800

    Create exercise12.py

---
+++ exercise12.py
@@ -0,0 +1,13 @@
+def list_end(given_list):
+    first_last = []
+    
+    first_last.append(given_list[0])
+    
+    first_last.append(given_list[-1])
+    
+    print(first_last)
+
+a = [5, 10, 15, 20, 25]
+
+list_end(a)
+        
