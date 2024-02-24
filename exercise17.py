2b99e9e53eaf242236267a629c8be3251d39a68f
Glasteroid <kennethangeles880@yahoo.com>
2024-02-24 11:56:15 -0800

    Update exercise17.py

--- exercise17.py
+++ exercise17.py
@@ -5,5 +5,25 @@ url = 'https://www.nytimes.com/'
 r = requests.get(url)
 r_html = r.text
 
-soup = BeautifulSoup(r_html)
-title = soup.find('span', 'articletitle').string
+soup = BeautifulSoup(r_html, 'lxml')
+
+class_names = [
+    "indicate-hover",
+    "css-vf1hbp",
+    "css-66vf3i",
+    "css-1gb49m4",
+    "css-1j7a2zk",
+    "css-1qmga95"
+]
+
+title = soup.find_all(class_=class_names)
+
+titles = []
+
+for t in title:
+    titles.append(t.text)
+    
+print(titles)
+    
+
+
