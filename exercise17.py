820a9febaf7c6227b823beb05b4f27179ad06309
Glasteroid <kennethangeles880@yahoo.com>
2024-02-24 11:59:00 -0800

    Update exercise17.py

--- exercise17.py
+++ exercise17.py
@@ -7,16 +7,7 @@ r_html = r.text
 
 soup = BeautifulSoup(r_html, 'lxml')
 
-class_names = [
-    "indicate-hover",
-    "css-vf1hbp",
-    "css-66vf3i",
-    "css-1gb49m4",
-    "css-1j7a2zk",
-    "css-1qmga95"
-]
-
-title = soup.find_all(class_=class_names)
+title = soup.find_all(class_="indicate-hover")
 
 titles = []
 
