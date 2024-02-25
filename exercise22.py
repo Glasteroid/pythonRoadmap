c22fc81c7f8bfc8a636094c65967b94109af5f7a
Glasteroid <kennethangeles880@yahoo.com>
2024-02-24 21:58:50 -0800

    Update exercise22.py

--- exercise22.py
+++ exercise22.py
@@ -1,2 +1,15 @@
+name_counts = {}
+    
 with open('nameslist.txt', 'r') as open_file:
-    all_text = open_file.read()
+    for line in open_file:
+        name = line.strip()
+        
+        if name in name_counts:
+            name_counts[name] += 1
+            
+        else:
+            name_counts[name] = 1
+            
+for x in name_counts.items():
+    print(x)
+             
